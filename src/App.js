a0a5321a251ac3c2be42d8f86b1ec68253a9a301
sagar1976 <narvekar.sagar@gmail.com>
2019-09-15 09:10:55 -0700

    Add.js file updated for Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
-
+import Header from "./Header"
 
 class App extends Component{
   render() {
     return(
       <div>
-    <div>
-      Phone directory
-    </div>
+      <Header/>
     <button> Add </button>
     <div>
       <span>Name</span><br/>
